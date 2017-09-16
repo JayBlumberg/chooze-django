bdd92a46790ba507465f64e238b810068d1fbc21
JayBlumberg <31857945+JayBlumberg@users.noreply.github.com>
2017-09-16 13:53:38 -0500

    Update views.py

--- app/views.py
+++ app/views.py
@@ -55,5 +55,5 @@ def about(request):
 
 def current_datetime(request):
     now = datetime.datetime.now()
-    html = "<html><body><It is now %s.</body></html> % now
+    html = "<html><body><It is now %s.</body></html>" % now
     return HttpResonse(html)
