a520fcdaa6873cef160e624349487f8bbaf7a363
JayBlumberg <31857945+JayBlumberg@users.noreply.github.com>
2017-09-16 14:23:46 -0500

    Update marc2.py

--- app/marc2.py
+++ app/marc2.py
@@ -3,6 +3,6 @@ def function1(var1):
     #insert python code here to code response
     a = "hello"
     b = "world"
-    
-    result = a + " " + world
+    result = a + " " + b
+        
     return result
